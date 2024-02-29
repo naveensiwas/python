d7725b89ccaa83a4191996f939f1fe459fd302c3
naveensiwas <naveenrbit@gmail.com>
2024-02-29 08:01:07 +0530

    local and global variable.

---
+++ 21.local_global_variables.py
@@ -0,0 +1,52 @@
+# Python Local Variables
+# Example : 1
+
+def greet():
+    msg_hello = 'Hello'
+    print('Local variable : ', msg_hello)
+
+
+greet()
+
+# try to access message variable
+# print(msg_hello) #NameError: name 'msg_hello' is not defined
+
+
+# Python Global Variables
+# Example : 1
+
+# declare global variable
+msg_hi = 'Hi'
+
+
+def greet_one():
+    print('Local variable : ', msg_hi)
+
+
+greet_one()
+print('Global variable : ', msg_hi)
+
+# Example : 2
+# declare global variable
+msg_bye = 'Bye'
+
+
+def greet_two():
+    print('Local variable : ', msg_bye)
+    # msg_bye = 'Bye Bye' #UnboundLocalError: local variable 'msg_bye' referenced before assignment
+
+
+greet_two()
+
+# Example : 3
+# declare global variable
+msg_bye_bye = 'Bye Bye'
+
+
+def greet_three():
+    global msg_bye_bye
+    msg_bye_bye = 'Bye Bye Updated'
+    print('Updated Global variable : ', msg_bye_bye)
+
+
+greet_three()
